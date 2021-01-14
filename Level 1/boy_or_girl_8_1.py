f0e8cfc4f2dc41477c15b17c3fe4842ed7152f83
Bahgat Ahmed <Bahgat-Ahmed67@hotmail.com>
2021-01-14 06:18:24 +0200

    problem 8 solutions

---
+++ Level 1/boy_or_girl_8_1.py
@@ -0,0 +1,10 @@
+name = input().strip()
+
+num_unique_chars = len(set(name))
+
+if num_unique_chars % 2 == 0:
+    # even number
+    print("she")
+else:
+    # even number
+    print("he")    
---
+++ Level 1/boy_or_girl_8_2.py
@@ -0,0 +1,1 @@
+print("boy") if len(set(input().strip())) % 2 == 1 else print("girl")
