84961b5b67f20ec388419b50a8441220aeaa2bdb
Bahgat Ahmed <Bahgat-Ahmed67@hotmail.com>
2021-02-22 07:50:22 +0200

    Updated part of problem 42 solution

--- Level 1/the_seasonal_war_42_1.py
+++ Level 1/the_seasonal_war_42_1.py
@@ -1,22 +1,15 @@
-class Node():
-    def __init__(self, value):
-        self.value = 1
-        self.right = None
-        self.upper_right = None
-        self.up = None
-        self.upper_left = None
-        self.left = None
-        self.down_left = None
-        self.down = None
-        self.down_right = None
+# TO BE CONTINUED
+
+import numpy as np
 
 # reading the input file
-with open('file_name.txt', 'r') as f:
+with open('../input/sample-text-file/text_file_42.txt', 'r') as f:
     text = f.read().split('\n')
 
 # getting images dimensions, and pixels into separate lists
 img_dims, img_pixels = list(), list()
 dim_flag = True
+img_pixel = list()
 for line in text:
     if dim_flag:
         img_dims.append(line)
@@ -31,8 +24,31 @@ for line in text:
             img_pixel = list()
             dim_flag = True
 
+valid_dx = [1, 1, 0, -1, -1, -1, 0, 1]
+valid_dy = [0, 1, 1, 1, 0, -1, -1, -1]
+
+def valid(i, j, n):
+    return (i >= 0 and j >= 0  and i < n and j < n)
+
+def check_eagle(i, j, n):
+
+    if seen[i][j]:
+        return
+
+    seen[i][j] = 1
+
+    for idx in range(8):
+        dx = i + valid_dx[idx]
+        dy = j + valid_dy[idx]
+        if valid(dx, dy, n):
+            check_eagle(dx, dy, n)
+
 for idx, img in enumerate(img_pixels):
     num_eagles = 0
-    for i in range(img_dims[idx]):
-        for j in range(img_dims[idx]):
-            if img[i][j] == 1: # TO BE CONTINUED
+    img_dim = int(img_dims[idx])
+    seen = np.zeros((img_dim, img_dim))
+    for i in range(img_dim):
+        for j in range(img_dim):
+            if img[i][j] == 1:
+                check_eagle(i, j, img_dim)
+                num_eagles += 1
