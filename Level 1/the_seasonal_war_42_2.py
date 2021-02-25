41d935b602e953e161543637bca3f91579c170ae
Bahgat Ahmed <Bahgat-Ahmed67@hotmail.com>
2021-02-25 20:58:05 +0200

    problem 42 same solution not reading from a file
    It is the same as the first solution but it is not reading from a file, and not writing to a file.

---
+++ Level 1/the_seasonal_war_42_2.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+def valid(i, j, n):
+    return (i >= 0 and j >= 0  and i < n and j < n)
+
+def check_eagle(ii, jj, n):
+
+    seen[ii][jj] = 1
+
+    for idx in range(8):
+        dx = ii + valid_dx[idx]
+        dy = jj + valid_dy[idx]
+        if valid(dx, dy, n) and (seen[dx][dy] == 0) and (int(img[dx][dy]) == 1):
+
+            check_eagle(dx, dy, n)
+
+if __name__ == '__main__':
+
+    img_dims, img_pixels = list(), list()
+    dim_flag = True
+    img_pixel = list()
+
+    while True:
+        try:
+            line = input()
+
+            if dim_flag:
+                img_dims.append(line)
+                stop_after = int(line)
+                counter = 0
+                dim_flag = False
+            else:
+                img_pixel.append(line)
+                counter += 1
+                if counter == stop_after:
+                    img_pixels.append(img_pixel)
+                    img_pixel = list()
+                    dim_flag = True
+
+        except:
+            break
+
+    valid_dx = [1, 1, 0, -1, -1, -1, 0, 1]
+    valid_dy = [0, 1, 1, 1, 0, -1, -1, -1]
+
+
+    for idx, img in enumerate(img_pixels):
+        num_eagles = 0
+        img_dim = int(img_dims[idx])
+        seen = np.zeros((img_dim, img_dim))
+        for i in range(img_dim):
+            for j in range(img_dim):
+                if (int(img[i][j]) == 1) and (seen[i][j] == 0):
+
+                    check_eagle(i, j, img_dim)
+                    num_eagles += 1
+
+        print(f"Image number {idx+1} contains {num_eagles} war eagles.")
