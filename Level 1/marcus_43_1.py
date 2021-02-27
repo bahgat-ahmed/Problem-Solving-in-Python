ee19f17c32e054c603e7b6f1d44f2ebf2ce60b98
Bahgat Ahmed <Bahgat-Ahmed67@hotmail.com>
2021-02-27 08:23:06 +0200

    problem 43 solution

--- Level 1/marcus_43_1.py
+++ Level 1/marcus_43_1.py
@@ -1,11 +1,34 @@
 num_test_cases = int(input())
 
-outputs = list()
+path_letters = 'IEHOVA#'
+all_commands = list()
 
 for i in range(num_test_cases):
+    current_commands = list()
     m, n = map(int, input().split())
 
     path = list()
 
     for j in range(m):
         path.insert(0, list(input()))
+
+    current_pos = path[0].index('@')
+    idx = 0
+    for letter in path_letters:
+        if letter in path[idx]:
+            destination_pos = path[idx].index(letter)
+            if destination_pos > current_pos:
+                current_commands.append('right')
+            else:
+                current_commands.append('left')
+        else:
+            idx += 1
+            destination_pos = path[idx].index(letter)
+            current_commands.append('forth')
+
+        current_pos = destination_pos
+
+    all_commands.append(current_commands)
+
+for commands in all_commands:
+    print(*commands)
