84549d16c0154221bc7d7ffc823db5dfc3e87c9a
Bahgat Ahmed <Bahgat-Ahmed67@hotmail.com>
2021-03-02 22:38:55 +0200

    Updated problem 44 solution.
    Not finished yet.

--- Level 1/battleships_44_1.py
+++ Level 1/battleships_44_1.py
@@ -6,13 +6,13 @@ dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
 
 
-def is_valid_fn(i, j, n, seen, grid, ship_len):
-    if i > 0 and j > 0 and i < n and j < n and seen[i][j] == 0 and\
-     grid[i][j] == 'x' and ship_len+1 <= n/2:
-        seen[x][y] = 1
+def is_valid_fn(ii, jj, nn, seen_, grid_, ship_len_):
+    if ii >= 0 and jj >= 0 and ii < nn and jj < nn and seen_[ii][jj] == 0 and\
+     grid_[ii][jj] == 'x' and ((ship_len_+1) <= nn/2):
         return 1
     else:
         return 0
+
 
 output_print = list()
 
@@ -29,27 +29,36 @@ for idx in range(num_test_cases):
 
     for i in range(grid_dim):
         for j in range(grid_dim):
-            if seen[i][j] == 0 and grid[i][j] == 'x':
+            seen[i][j] = 1
+            if grid[i][j] == 'x':
                 alive_ships += 1
                 ship_len += 1
-                seen[i][j] = 1
 
                 for k in range(4):
                     x = i + dx[k]
                     y = j + dy[k]
+
                     if is_valid_fn(x, y, grid_dim, seen, grid, ship_len):
+                        seen[x][y] = 1
                         ship_len += 1
                         if dx[k]:
-                            for m in range(x, x + int(np.ceil(grid_dim/2)) + 1):
-                                if is_valid_fn(m+1, y, grid_dim, seen, grid, ship_len):
+                            for m in range(x, x + int(np.ceil(grid_dim/2))
+                                           + 1):
+                                if is_valid_fn(m+1, y, grid_dim, seen, grid,
+                                   ship_len):
+                                    seen[m+1][y] = 1
                                     ship_len += 1
-                                if is_valid_fn(m-1, y, grid_dim, seen, grid, ship_len):
+                                if is_valid_fn(m-1, y, grid_dim, seen, grid,
+                                   ship_len):
+                                    seen[m-1][y] = 1
                                     ship_len += 1
                         else:
                             for n in range(y, y + int(np.ceil(grid_dim/2)) + 1):
                                 if is_valid_fn(x, n+1, grid_dim, seen, grid, ship_len):
+                                    seen[x][n+1] = 1
                                     ship_len += 1
                                 if is_valid_fn(x, n-1, grid_dim, seen, grid, ship_len):
+                                    seen[x][n-1] = 1
                                     ship_len += 1
                         break
 
