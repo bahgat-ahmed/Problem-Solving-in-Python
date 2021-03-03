ba4b310cef01feacedf1fc753e18d850454b7a37
Bahgat Ahmed <Bahgat-Ahmed67@hotmail.com>
2021-03-03 09:06:14 +0200

    Problem 44 first solution

--- Level 1/battleships_44_1.py
+++ Level 1/battleships_44_1.py
@@ -8,7 +8,7 @@ dy = [1, -1, 0, 0]
 
 def is_valid_fn(ii, jj, nn, seen_, grid_, ship_len_):
     if ii >= 0 and jj >= 0 and ii < nn and jj < nn and seen_[ii][jj] == 0 and\
-     grid_[ii][jj] == 'x' and ((ship_len_+1) <= nn/2):
+     (grid_[ii][jj] == 'x' or grid_[ii][jj] == '@') and (ship_len_+1 <= nn/2):
         return 1
     else:
         return 0
@@ -16,11 +16,10 @@ def is_valid_fn(ii, jj, nn, seen_, grid_, ship_len_):
 
 output_print = list()
 
-for idx in range(num_test_cases):
+for idxx in range(num_test_cases):
     grid_dim = int(input())
     grid = list()
     alive_ships = 0
-    ship_len = 0
 
     for _ in range(grid_dim):
         grid.append(list(input()))
@@ -29,8 +28,9 @@ for idx in range(num_test_cases):
 
     for i in range(grid_dim):
         for j in range(grid_dim):
-            seen[i][j] = 1
-            if grid[i][j] == 'x':
+            ship_len = 0
+            if (grid[i][j] == 'x') and (seen[i][j] == 0):
+                seen[i][j] = 1
                 alive_ships += 1
                 ship_len += 1
 
@@ -41,28 +41,37 @@ for idx in range(num_test_cases):
                     if is_valid_fn(x, y, grid_dim, seen, grid, ship_len):
                         seen[x][y] = 1
                         ship_len += 1
-                        if dx[k]:
-                            for m in range(x, x + int(np.ceil(grid_dim/2))
-                                           + 1):
-                                if is_valid_fn(m+1, y, grid_dim, seen, grid,
-                                   ship_len):
-                                    seen[m+1][y] = 1
+                        enter_1, enter_2 = True, True
+                        if abs(dx[k]):
+                            for idx, m in enumerate(range(x+1, x + int(np.ceil(grid_dim/2) + 1)), 1):
+                                if is_valid_fn(m, y, grid_dim, seen, grid,
+                                   ship_len) and enter_1:
+                                    seen[m][y] = 1
                                     ship_len += 1
-                                if is_valid_fn(m-1, y, grid_dim, seen, grid,
-                                   ship_len):
-                                    seen[m-1][y] = 1
+                                else:
+                                    enter_1 = False
+                                if is_valid_fn(x-idx, y, grid_dim, seen, grid,
+                                   ship_len) and enter_2:
+                                    seen[x-idx][y] = 1
                                     ship_len += 1
+                                else:
+                                    enter_2 = False
                         else:
-                            for n in range(y, y + int(np.ceil(grid_dim/2)) + 1):
-                                if is_valid_fn(x, n+1, grid_dim, seen, grid, ship_len):
-                                    seen[x][n+1] = 1
+                            for idx, n in enumerate(range(y+1, y + int(np.ceil(grid_dim/2)) + 1), 1):
+                                if is_valid_fn(x, n, grid_dim, seen, grid, ship_len) and enter_1:
+                                    seen[x][n] = 1
                                     ship_len += 1
-                                if is_valid_fn(x, n-1, grid_dim, seen, grid, ship_len):
-                                    seen[x][n-1] = 1
+                                else:
+                                    enter_1 = False
+                                if is_valid_fn(x, y-idx, grid_dim, seen, grid, ship_len) and enter_2:
+                                    seen[x][y-idx] = 1
                                     ship_len += 1
+                                else:
+                                    enter_2 = False
                         break
 
-    output_print.append(f"Case {idx+1}: {alive_ships}")
+    output_print.append(f"Case {idxx+1}: {alive_ships}")
 
 for output in output_print:
     print(output)
+    
