04bcabf9feca2c67ecdb499c54b4392104d4d904
Bahgat Ahmed <Bahgat-Ahmed67@hotmail.com>
2021-03-05 07:12:40 +0200

    removed unneeded two lines

--- Level 1/battleships_44_2.py
+++ Level 1/battleships_44_2.py
@@ -2,10 +2,6 @@ import numpy as np
 
 num_test_cases = int(input())
 
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
-
-
 def search(ii, jj, nn, seen_, grid_):
     if ii >= 0 and jj >= 0 and ii < nn and jj < nn and seen_[ii][jj] == 0 and\
      (grid_[ii][jj] == 'x' or grid_[ii][jj] == '@'):
