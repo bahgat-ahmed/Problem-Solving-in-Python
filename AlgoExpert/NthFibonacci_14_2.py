36bcc660fc3f5023f275857a73e6ed0aa647326c
Bahgat Ahmed <Bahgat-Ahmed67@hotmail.com>
2021-10-11 06:17:34 +0200

    problem 14 second solution

---
+++ AlgoExpert/NthFibonacci_14_2.py
@@ -0,0 +1,8 @@
+# recursive solution with caching or memoization
+# O (n) time | O(n) space - where n is the input number
+def getNthFib(n, memoize={1: 0, 2: 1}):
+	if n in memoize:
+		return memoize[n]
+	else:
+		memoize[n] = getNthFib(n - 1, memoize) + getNthFib(n - 2, memoize)
+		return memoize[n]
