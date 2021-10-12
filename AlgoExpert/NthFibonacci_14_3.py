5d53b2ac271532c34ace4e7a24a8bdff352d68f9
Bahgat Ahmed <Bahgat-Ahmed67@hotmail.com>
2021-10-12 06:17:38 +0200

    problem 14 third solution

---
+++ AlgoExpert/NthFibonacci_14_3.py
@@ -0,0 +1,15 @@
+# iterative solution
+# O(n) time | O(1) space - where n is the input number 
+def getNthFib(n):
+    if n < 3:
+		return n - 1
+	fib_n_2 = 1
+	fib_n_1 = 0
+	counter = 3
+	while counter <= n:
+		current_fib = fib_n_2 + fib_n_1
+		fib_n_1 = fib_n_2
+		fib_n_2 = current_fib
+		counter += 1
+		
+	return current_fib
