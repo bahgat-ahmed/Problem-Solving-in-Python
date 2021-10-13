1405c1b500e4b1701a00ec904581fb784b44969d
Bahgat Ahmed <Bahgat-Ahmed67@hotmail.com>
2021-10-13 06:14:44 +0200

    Update ProductSum_15_1.py

--- AlgoExpert/ProductSum_15_1.py
+++ AlgoExpert/ProductSum_15_1.py
@@ -2,11 +2,11 @@
 # including "inner" arrays and their sub-elements, and d is the greatest depth
 # of "inner" arrays in the input array
 def productSum(array, depth=1):
-	product_sum = 0
+    product_sum = 0
     for element in array:
-		if type(element) is int:
-			product_sum += element
-		else:
-			product_sum += productSum(element, depth + 1)
-			
-	return product_sum * depth
+        if type(element) is int:
+	    product_sum += element
+	else:
+	    product_sum += productSum(element, depth + 1)
+
+    return product_sum * depth
