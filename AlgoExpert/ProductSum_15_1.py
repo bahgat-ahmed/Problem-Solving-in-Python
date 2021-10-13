00957bcf0f14e66061d43006f5d4ab1c41c7d119
Bahgat Ahmed <Bahgat-Ahmed67@hotmail.com>
2021-10-13 06:13:01 +0200

    problem 15 solution

---
+++ AlgoExpert/ProductSum_15_1.py
@@ -0,0 +1,12 @@
+# O(n) time | O(d) space - where n is the number of elements in the array,
+# including "inner" arrays and their sub-elements, and d is the greatest depth
+# of "inner" arrays in the input array
+def productSum(array, depth=1):
+	product_sum = 0
+    for element in array:
+		if type(element) is int:
+			product_sum += element
+		else:
+			product_sum += productSum(element, depth + 1)
+			
+	return product_sum * depth
