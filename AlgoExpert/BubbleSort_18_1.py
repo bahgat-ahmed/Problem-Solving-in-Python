a1563036639000ce45ab49f71585ad5fc00eb73d
Bahgat Ahmed <Bahgat-Ahmed67@hotmail.com>
2021-10-28 07:19:09 +0200

    Update BubbleSort_18_1.py

--- AlgoExpert/BubbleSort_18_1.py
+++ AlgoExpert/BubbleSort_18_1.py
@@ -2,14 +2,14 @@
 # Best: O(n) time | O(1) space - when input array is already sorted
 def bubbleSort(array):
     for i in range(len(array)):
-		swap_happened = False
-		for j in range(len(array) - 1 - i):
-			if array[j+1] < array[j]:
-				temp = array[j]
-				array[j] = array[j+1]
-				array[j+1] = temp
-				swap_happened = True
-		if swap_happened is False:
-			break
+	swap_happened = False
+	for j in range(len(array) - 1 - i):
+		if array[j+1] < array[j]:
+			temp = array[j]
+			array[j] = array[j+1]
+			array[j+1] = temp
+			swap_happened = True
+	if swap_happened is False:
+		break
 				
-	return array
+    return array
