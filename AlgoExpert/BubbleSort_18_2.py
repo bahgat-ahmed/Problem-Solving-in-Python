f13b0b061c16a9655c116145d5ca702b74a9c831
Bahgat Ahmed <Bahgat-Ahmed67@hotmail.com>
2021-10-29 20:38:49 +0200

    Update BubbleSort_18_2.py

--- AlgoExpert/BubbleSort_18_2.py
+++ AlgoExpert/BubbleSort_18_2.py
@@ -2,12 +2,12 @@
 # Best: O(n) time | O(1) space - when input array is already sorted
 def bubbleSort(array):
     is_sorted = False
-	counter = 0
-	while not is_sorted:
-		is_sorted = True
-		for i in range(len(array) - 1 - counter):
-			if array[i + 1] < array[i]:
-				array[i + 1], array[i] = array[i], array[i + 1]
-				is_sorted = False
-		counter += 1
-	return array
+    counter = 0
+    while not is_sorted:
+	is_sorted = True
+	for i in range(len(array) - 1 - counter):
+		if array[i + 1] < array[i]:
+			array[i + 1], array[i] = array[i], array[i + 1]
+			is_sorted = False
+	counter += 1
+    return array
