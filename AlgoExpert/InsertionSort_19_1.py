f52350662f358b7c30f91c72b5115f06a513d14d
Bahgat Ahmed <Bahgat-Ahmed67@hotmail.com>
2021-10-30 15:29:55 +0200

    Update InsertionSort_19_1.py

--- AlgoExpert/InsertionSort_19_1.py
+++ AlgoExpert/InsertionSort_19_1.py
@@ -2,10 +2,10 @@
 
 def insertionSort(array):
     for i in range(1, len(array)):
-		for j in reversed(range(i)):
-			if array[j+1] < array[j]:
-				array[j], array[j+1] = array[j+1], array[j]
-			else:
-				break
+	for j in reversed(range(i)):
+		if array[j+1] < array[j]:
+			array[j], array[j+1] = array[j+1], array[j]
+		else:
+			break
 				
-	return array
+    return array
