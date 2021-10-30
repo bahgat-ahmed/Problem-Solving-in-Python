b6c6c13e60e806335863b1475a60700cfd86400d
Bahgat Ahmed <Bahgat-Ahmed67@hotmail.com>
2021-10-30 15:28:25 +0200

    problem 19 first solution

---
+++ AlgoExpert/InsertionSort_19_1.py
@@ -0,0 +1,11 @@
+# O(n^2) time | O(1) space - where n it the length of the input array
+
+def insertionSort(array):
+    for i in range(1, len(array)):
+		for j in reversed(range(i)):
+			if array[j+1] < array[j]:
+				array[j], array[j+1] = array[j+1], array[j]
+			else:
+				break
+				
+	return array
