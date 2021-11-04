7c1e86e2513c8e2073f61b9b636be70bc6841214
Bahgat Ahmed <Bahgat-Ahmed67@hotmail.com>
2021-11-04 08:51:29 +0200

    Update SelectionSort_20_1.py

--- AlgoExpert/SelectionSort_20_1.py
+++ AlgoExpert/SelectionSort_20_1.py
@@ -1,13 +1,13 @@
 # O(n^2) time | O(1) space - where n is the length of the input array
 def selectionSort(array):
     for i in range(len(array)):
-		smallest = i
-		is_sorted = True
-		for j in range(i + 1, len(array)):
-			if array[smallest] > array[j]:
-				smallest = j
-				is_sorted = False
-		if is_sorted is False:
-			array[smallest], array[i] = array[i], array[smallest]
+	smallest = i
+	is_sorted = True
+	for j in range(i + 1, len(array)):
+		if array[smallest] > array[j]:
+			smallest = j
+			is_sorted = False
+	if is_sorted is False:
+		array[smallest], array[i] = array[i], array[smallest]
 
-	return array
+    return array
