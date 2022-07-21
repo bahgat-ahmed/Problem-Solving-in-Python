673ac45f9679d0bdc8d9d09c270561e2ad605377
Bahgat Ahmed <Bahgat-Ahmed67@hotmail.com>
2022-07-21 05:01:49 +0200

    problem 38 solutions

---
+++ AlgoExpert/MinHeightBST_38_1.py
@@ -0,0 +1,40 @@
+# O(nlog(n)) time | O(n) space - where n is the length of the array
+def minHeightBst(array):
+    return constructMinHeightBst(array, None, 0, len(array) - 1)
+
+
+def constructMinHeightBst(array, bst, startIdx, endIdx):
+    if startIdx > endIdx:
+        return bst
+
+    midIdx = (startIdx + endIdx) // 2
+    valueToAdd = array[midIdx]
+
+    if bst is None:
+        bst = BST(valueToAdd)
+    else:
+        bst.insert(valueToAdd)
+
+    constructMinHeightBst(array, bst, startIdx, midIdx - 1)
+    constructMinHeightBst(array, bst, midIdx + 1, endIdx)
+
+    return bst
+    
+
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = BST(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = BST(value)
+            else:
+                self.right.insert(value)
